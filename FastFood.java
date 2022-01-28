f77377410e0d7919ead56466d35c79490ee5b697
Adeetee kekre <adeetee18kekre@gmail.com>
2022-01-28 14:17:17 +0530

    FastFood Added

---
+++ FastFood.java
@@ -0,0 +1,13 @@
+public class FastFood {
+
+    public static void main(String gg[])
+    {
+System.out.println("PIZZA");
+System.out.println("FRENCH FRIES");
+System.out.println("CHOLE KULCHE");
+System.out.println("PAV BHAJI");
+
+    }
+
+    
+}
